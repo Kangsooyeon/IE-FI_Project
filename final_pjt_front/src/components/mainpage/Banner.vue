0af30ea9d1881e761020a82f59af6b93ec2b680d
guniFF <sky4tony@naver.com>
2024-05-16 12:48:18 +0900

    Create QuickMenu

--- final_pjt_front/src/components/mainpage/Banner.vue
+++ final_pjt_front/src/components/mainpage/Banner.vue
@@ -1,5 +1,32 @@
 <template>
-    <div>Banner</div>
+    <div class="carousel-container">
+        <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-bs-target="#carouselExample" data-bs-slide-to="0" class="active"></li>
+                <li data-bs-target="#carouselExample" data-bs-slide-to="1"></li>
+                <li data-bs-target="#carouselExample" data-bs-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="@/assets/bannerexample/ex1.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="@/assets/bannerexample/ex2.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="@/assets/bannerexample/ex3.jpg" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
 </template>
 
 
@@ -9,5 +36,19 @@
 
 
 <style scoped>
+.carousel-container{
+    min-width: 395px;
+}
+.carousel-inner {
+    max-height: 500px;
+    max-width: 980px;
+    border-radius: 5px;
+}
+.carousel-indicators li {
+    list-style: none;
+}
+.carousel-item img {
+    border-radius: 5px;
+}
 
 </style>
--- final_pjt_front/src/components/mainpage/QuickMenu.vue
+++ final_pjt_front/src/components/mainpage/QuickMenu.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>QuickMenu</div>
+    <div>
+        <div class="d-flex flex-row my-5">	
+            <h2>빠른 메뉴</h2>
+            <ul class="quick-menu-list d-flex flex-row">
+                <li class="quick-menu-item"><a href=""><div><img src="https://www.wooribank.com/img/common/etc/bg_btn/icon_set_011.png" alt="예금·적금"><p>예금·적금</p></div></a></li>
+                <li class="quick-menu-item"><a href=""><div><img src="https://www.wooribank.com/img/common/etc/bg_btn/icon_set_022.png" alt="추천상품"><p>추천상품</p></div></a></li>
+                <li class="quick-menu-item"><a href=""><div><img src="https://www.wooribank.com/img/common/etc/bg_btn/icon_set_033.png" alt="은행찾기"><p>은행찾기</p></div></a></li>	
+                <li class="quick-menu-item"><a href=""><div><img src="https://www.wooribank.com/img/common/etc/bg_btn/icon_set_044.png" alt="환율계산"><p>환율계산</p></div></a></li>
+                <li class="quick-menu-item"><a href=""><div><img src="https://www.wooribank.com/img/common/etc/bg_btn/icon_set_055.png" alt="커뮤니티"><p>커뮤니티</p></div></a></li>
+                <li class="quick-menu-item"><a href=""><div><img src="https://www.wooribank.com/img/common/etc/bg_btn/icon_set_088.png" alt="QnA"><p>QnA</p></div></a></li>
+            </ul>
+        </div>
+    </div> 
 </template>
 
 
@@ -9,5 +21,14 @@
 
 
 <style scoped>
+.quick-menu-list{
+    list-style: none;
+}
+.quick-menu-item{
+    background: transparent;
+    border-radius: 50%;
+    color: rgb(0, 0, 0);
+    margin: 0 10px;
+}
 
 </style>

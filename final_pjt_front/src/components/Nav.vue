222e43b0bf942580996f6096d83cf61d3401491f
guniFF <sky4tony@naver.com>
2024-05-16 10:42:11 +0900

    Update Nav-create offcanvas

--- final_pjt_front/src/components/Nav.vue
+++ final_pjt_front/src/components/Nav.vue
@@ -8,7 +8,7 @@
                     <a class="navbar-brand btn custom-btn register-btn" href="#">회원가입</a>
                 </div>
 
-                
+                <!-- Nav링크 -->
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
@@ -28,23 +28,46 @@
                         </li>
                     </ul>
                 </div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample">
+                
+                <!-- 햄버거버튼 -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                
 
-            </div>
+                <!-- 햄버거 메뉴 -->
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body d-lg-none">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+    
+                    
+                </div>
+
         </nav>
     </div>
 </template>
 
 <script setup>
+
 </script>
 
 <style scoped>
 .navbar {
     background-color: white !important;
     max-width: 980px;
+    min-width: 395px;
     margin: auto;
 }
 

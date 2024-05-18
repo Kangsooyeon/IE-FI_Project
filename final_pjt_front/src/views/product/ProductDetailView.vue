6440a86be3898228c8e12165692633e6b729304b
dgkim <sky4tony@naver.com>
2024-05-19 03:18:28 +0900

    Handle Detailpage error

--- final_pjt_front/src/views/product/ProductDetailView.vue
+++ final_pjt_front/src/views/product/ProductDetailView.vue
@@ -1,7 +1,10 @@
+<script>
+// console.log(productDetail);
+</script>
 <template>
   <div>
-    {{ productDetail }}
-    <h1>{{ productDetail.product.fin_prdt_nm }}</h1>
+    {{ productDetail.product?.fin_prdt_nm }}
+    <!-- <h1>{{ productDetail.product.fin_prdt_nm }}</h1> -->
     <!-- <p>{{ productDetail.product.kor_co_nm }}</p>
     <p>{{ productDetail.product.etc_note }}</p>
     <p>{{ productDetail.product.join_member }}</p>
@@ -24,48 +27,49 @@
   </div>
 </template>
 
-
 <script setup>
-  import ProductDescription from '@/components/productdetail/ProductDescription.vue';
-  import { ref, onMounted } from 'vue'
-  import { useProjectStore } from '@/stores/project'; 
-  import { useRoute } from 'vue-router';
-  import axios from 'axios';
+import ProductDescription from '@/components/productdetail/ProductDescription.vue';
+import { ref, onMounted } from 'vue';
+import { useProjectStore } from '@/stores/project';
+import { useRoute } from 'vue-router';
+import axios from 'axios';
 
-  const store = useProjectStore();
-  const route = useRoute();
+const store = useProjectStore();
+const route = useRoute();
 
-  const productId = ref(route.params.fin_prdt_cd);
-  const productAll = ref([]);
-  const productDetail = ref({});
+const productId = ref(route.params.fin_prdt_cd);
+const productAll = ref([]);
+const productDetail = ref({});
 
-  onMounted(() => {
-    axios({
-              method: 'get',
-              url:'http://127.0.0.1:8000/products/products-all/'
-            }).then((res) => {
-              productAll.value=res.data
-              let dors="deposit_product"
-              const productDetailTMP = productAll.value.find((el) => {
-                if(el['deposit_product']!==undefined && el.deposit_product.fin_prdt_cd === productId.value){
-                  return el
-                }
-                else if(el['saving_product']!==undefined && el.saving_product.fin_prdt_cd === productId.value){
-                  dors="saving_product"
-                  return el
-                }
-
-              })
-              productDetail.value={
-                product:productDetailTMP[dors],
-                options:productDetailTMP.options
-              }
-              console.log(productId.value);
-              console.log(productDetail.value);
-              })
+onMounted(() => {
+  axios({
+    method: 'get',
+    url: 'http://127.0.0.1:8000/products/products-all/',
+  }).then((res) => {
+    productAll.value = res.data;
+    let dors = 'deposit_product';
+    const productDetailTMP = productAll.value.find((el) => {
+      if (
+        el['deposit_product'] !== undefined &&
+        el.deposit_product.fin_prdt_cd === productId.value
+      ) {
+        return el;
+      } else if (
+        el['saving_product'] !== undefined &&
+        el.saving_product.fin_prdt_cd === productId.value
+      ) {
+        dors = 'saving_product';
+        return el;
+      }
+    });
+    productDetail.value = {
+      product: productDetailTMP[dors],
+      options: productDetailTMP.options,
+    };
+    console.log(productId.value);
+    console.log(productDetail.value);
   });
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

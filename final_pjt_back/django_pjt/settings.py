2350dfae85c39e69b8188577f66378db3ab0a102
unknown <sky4tony@naver.com>
2024-07-07 05:00:55 +0900

    ALLOWED_HOSTS update

--- final_pjt_back/django_pjt/settings.py
+++ final_pjt_back/django_pjt/settings.py
@@ -42,7 +42,8 @@ API_KEY_NAVER_ID = env('API_KEY_NAVER_ID')
 API_KEY_NAVER_SECRET = env('API_KEY_NAVER_SECRET')
 
 ALLOWED_HOSTS = [
-    'ec2-54-180-133-25.ap-northeast-2.compute.amazonaws.com'
+    'ec2-54-180-133-25.ap-northeast-2.compute.amazonaws.com',
+    'ec2-54-180-133-25.ap-northeast-2.compute.amazonaws.com:8000'
 ]
 
 
